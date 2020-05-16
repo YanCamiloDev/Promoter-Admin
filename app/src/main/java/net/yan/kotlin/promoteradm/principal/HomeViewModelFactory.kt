b2417df74134ed46eb7c2b49a2ca925f0eb831b9
YanCamiloDev <59901791+YanCamiloDev@users.noreply.github.com>
2020-05-16 19:54:23 -0300

    Promoter Admin

--- app/src/main/java/net/yan/kotlin/promoteradm/principal/HomeViewModelFactory.kt
+++ app/src/main/java/net/yan/kotlin/promoteradm/principal/HomeViewModelFactory.kt
@@ -4,18 +4,16 @@ import android.content.res.Resources
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import net.yan.kotlin.promoteradm.data.FirebaseHelper
-import net.yan.kotlin.promoteradm.databinding.FragmentHomeBinding
 
 
 class HomeViewModelFactory(
     private val dataSource: FirebaseHelper,
-    private val binding: FragmentHomeBinding,
     private val resources: Resources
 ) : ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
-            return HomeViewModel(dataSource, binding, resources) as T
+            return HomeViewModel(dataSource, resources) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
